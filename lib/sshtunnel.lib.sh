22ce79a2240edc87ada03714a4a315eb39013c40
hideroot <hideroot@gmail.com>
2022-02-20 20:46:27 +0300

    add pack_dir add pack_is_change

--- lib/sshtunnel.lib.sh
+++ lib/sshtunnel.lib.sh
@@ -8,13 +8,13 @@ function ssh_tunnel_R {
 # $5  - RemoteAddress
 # Example
 # ssh_tunnel_R 22 2222 remuser server.com 127.0.0.1:
-    local CMD="ssh -oStrictHostKeyChecking=no -oTCPKeepAlive=yes -oServerAliveInterval=60 -oServerAliveCountMax=3 -f -N -T -i/root/.ssh/id_rsa -R $5$2:127.0.0.1>    pgrep -f "$CMD" &>/dev/null || $CMD
+    local CMD="ssh -oStrictHostKeyChecking=no -oTCPKeepAlive=yes -oServerAliveInterval=60 -oServerAliveCountMax=3 -f -N -T -i/root/.ssh/id_rsa -R $5$2:127.0.0.1 > &>/dev/null"
     RES="$(ssh $3@$4 testport $2)"
     if [ "$RES" = "ONLINE" ]; then
       echo  "Done $1->$2 $4"
     else
        echo "Restart Connect $1->$2 $4"
-       pkill -f -x "$CMD"
+       pkill -f -x $CMD
        $CMD
     fi
 }
